c881b6bcef9a6ebdbe35c95f8f5e9acca265e8b1
codeproj01 <cliftonmpls@comcast.net>
2019-10-18 21:02:08 -0500

    Re-deploy

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,11 +1,31 @@
 var topics = ["muhammad ali", "elvis", "michael jackson", "james bond", "bruce lee", "prince", "michael jordan", "goofy"];
 
+
+// Initialize div before rendering buttons prior to adding new gifs
+function renderButtons(){
+	document.getElementById("gifsButtons").innerHTML = "";
+
+	for (var i = 0 ; i < topics.length; i++) {
+	//Dynamically generate buttons.
+		let a = document.createElement("button");
+			a.classList.add("gifs");
+			a.setAttribute("data-gifs", topics[i]);
+			a.innerHTML = topics[i];
+
+			document.getElementById("gifsButtons").append(a);
+			
+		}	
+		document.querySelectorAll("button").forEach(function (button) {
+			button.addEventListener("click", getGifs);
+		});
+	}
+
 //Function for getting the GIFs and dumping the GIF in to the div.
 
 function getGifs() {
 
-	const emotion = this.getAttribute("data-emotion");
-	const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+	const gifs = this.getAttribute("data-gifs");
+	const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
 	
 	// Creating an fetch call for the specific gif button being clicked
 	fetch(queryURL)
@@ -21,24 +41,27 @@ function getGifs() {
 		for (var i = 0; i < results.length; i++) {
 			if (results[i].rating !== "r") {
 			var gifDiv = document.createElement("item");
+			var rating = results[i].rating;
 			var p = document.createElement("p").innerText = ("Rating:" + results[i].rating);
 
-			var rating = results[i].rating;
-
-			var emotionImage = document.createElement("img");
+			var gifsImage = document.createElement("img");
 
 			// Added div class to gif
-			emotionImage.classList.add("gif");
+			gifsImage.classList.add("gif");
 			gifDiv.classList.add("gif");
+        	gifsImage.classList.add("gif");
+        	gifsImage.setAttribute("src", results[i].images.original_still.url);
+        	gifsImage.setAttribute("data-state", "still");
+        	gifsImage.setAttribute("data-animate", results[i].images.original.url);
+        	gifsImage.setAttribute("data-still", results[i].images.original_still.url);
+        	gifsImage.addEventListener("click", toggleImage);
 
-		// animated gif: emotionImage.attr("src", results[i].images.fixed_height.url);
-		// still gif (fixed_height_still)
-		emotionImage.setAttribute("src", results[i].images.fixed_height_still.url);
+			gifsImage.setAttribute("src", results[i].images.fixed_height_still.url);
 
-		gifDiv.append(p);
-		gifDiv.append(emotionImage);
+			gifDiv.append(p);
+			gifDiv.append(gifsImage);
 		
-		document.getElementById("emotions").prepend(gifDiv);
+			document.getElementById("gifsDump").prepend(gifDiv);
 		
 
 		}
@@ -47,50 +70,28 @@ function getGifs() {
 	
 };
 
-//starting and stopping gif
-
-// If the gif element inside ‘body’ is ever clicked then run the handler function.  
-// Using jQuery here for ease and simplicity.
-$('body').on('click', '.gif', function() {
-    let src = $(this).attr("src");
-    if($(this).hasClass('playing')){
-     //stop
-     $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
-     $(this).removeClass('playing');
-  } else {
-    //play
-    $(this).addClass('playing');
-    $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
-  }
-});
-
-// Initialize div before rendering buttons prior to adding new gifs
-function renderButtons(){
-	document.getElementById("emotionButtons").innerHTML = "";
-
-	for (var i = 0 ; i < topics.length; i++) {
-	//Dynamically generate buttons.
-		var a = document.createElement("button");
-			a.classList.add("emotion");
-			a.setAttribute("data-emotion", topics[i]);
-			a.innerHTML = topics[i];
-			a.classList.add("btn-info");
-			document.getElementById("emotionButtons").append(a);
-			
-		}	
+function toggleImage (event) {
+		const state = event.target.getAttribute("data-state");
+	
+		if (state === "still") {
+			event.target.setAttribute("src", event.target.getAttribute("data-animate"));
+			event.target.setAttribute("data-state", "animate");
+		} else {
+			event.target.setAttribute("src", event.target.getAttribute("data-still"));
+			event.target.setAttribute("data-state", "still");
+		}
 	}
+
 // Event handler for when a button is pushed.
-document.getElementById("addEmotion").addEventListener("click", function(e){
+document.getElementById("addGifs").addEventListener("click", function(e){
 	e.preventDefault();
 	// User input from the textbox
-	var emotion = document.getElementById("emotion-input").value.trim();
+	var gifs = document.getElementById("gifs-input").value.trim();
 
-	topics.push(emotion);
+	topics.push(gifs);
 
 	renderButtons();
 
 });
-
-$(document).on("click", ".emotion", getGifs);
 
 renderButtons();
